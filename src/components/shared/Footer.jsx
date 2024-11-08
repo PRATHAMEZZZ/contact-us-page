fb290a497970506931f0fe5fb42013606f69c33c
PrathameshTharali <prathamesh@kdigitalcurry.com>
2024-11-08 13:27:32 +0530

    initial commit

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -6,11 +6,11 @@ const footerLinks = [
   {
     title: "What We Do",
     links: [
-      { name: "Intro", url: "/intro" },
-      { name: "Home", url: "/home" },
-      { name: "Work", url: "/work" },
-      { name: "Pricing", url: "/pricing" },
-      { name: "Contact Us", url: "/contact-us" },
+      { name: "Intro", url: "https://agentc-contact-us.netlify.app/intro" },
+      { name: "Home", url: "https://agentc-contact-us.netlify.app" },
+      { name: "Work", url: "https://agentc-contact-us.netlify.app" },
+      { name: "Pricing", url: "https://agentc-contact-us.netlify.app" },
+      { name: "Contact Us", url: "https://agentc-contact-us.netlify.app" },
     ],
   },
   {
@@ -27,16 +27,13 @@ const Footer = () => {
   return (
     <footer className="bg-neutral-900 text-white py-20">
       <div className="container mx-auto px-6 md:px-12 lg:px-24">
-        {/* Flexbox container for the main content */}
         <div className="flex flex-col lg:flex-row gap-8 lg:justify-between">
-          {/* First Column (Company Info) */}
           <div className="lg:w-1/3">
             <h3 className="text-5xl font-semibold mb-4">dsgnr.</h3>
             <p className="text-[20px] sm:text-[24px] md:text-[24px] lg:text-[24px] font-medium leading-[35px] text-left my-10">
               Dsgnr. is an award-winning creative and design agency <br /> based
               in New York, USA.
             </p>
-
             <div className="flex items-center space-x-10 border border-1 border-[#FEC00A] min-w-[100px] px-8 py-4">
               <FaEnvelope size={40} />
               <a
@@ -47,7 +44,6 @@ const Footer = () => {
               </a>
             </div>
           </div>
-
           <div className="flex justify-between  flex-wrap gap-x-20">
             {footerLinks.map((column, index) => (
               <div key={index} className="mb-8 lg:mb-8">
